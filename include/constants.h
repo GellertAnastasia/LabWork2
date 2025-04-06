eb3a58c9ddda2669912fa65e12ba94b383b36ff9
GellertAnastasia <st130144@student.spbu.ru>
2025-04-07 00:15:54 +0300

    fix: constants

--- include/constants.h
+++ include/constants.h
@@ -1,9 +1,19 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
+#include <string>
 
 const int WIDTH = 10;
 const int HEIGHT = 10;
 
-const char EMPTY_CELL = '.';
+const std::string EMPTY_CELL = "◦";
+const std::string circled_numbers[] = {"➀", "➁", "➂", "➃", "➄", "➅", "➆", "➇", "➈", "➉"};
 
+const std::string forest_green = "\033[38;5;28m";
+const std::string azure = "\033[38;2;135;206;250m";
+const std::string reset = "\033[0m";
+
+const std::string MANA = "\033[38;5;129m✦\033[0m";
+const std::string MONEY = "\033[38;2;255;215;0m●\033[0m";
+const std::string HEALTH = "\033[31m♥\033[0m";
+const std::string POWER = "\033[38;5;240m⸸\033[0m";
 #endif
